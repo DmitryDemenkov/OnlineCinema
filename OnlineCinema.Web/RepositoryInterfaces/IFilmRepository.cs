d508365275905de735754ca7bdaee04ee0102cd0
DmitryDemenkov <demon.krut2002@mail.ru>
2022-05-29 11:06:32 +0300

    Добавлен репозиторий фильмов

---
+++ OnlineCinema.Web/RepositoryInterfaces/IFilmRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OnlineCinema.Web.Models;
+
+namespace OnlineCinema.Web.RepositoryInterfaces
+{
+    public interface IFilmRepository : IRepository<Film>
+    {
+        IEnumerable<Film> GetPopular();
+    }
+}
---
+++ OnlineCinema.Web/RepositoryInterfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineCinema.Web.RepositoryInterfaces
+{
+    public interface IRepository<T>
+    {
+        T GetById();
+    }
+}
