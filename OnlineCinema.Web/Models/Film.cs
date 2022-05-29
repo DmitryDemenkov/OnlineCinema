86f2d42e8278d80095cdda55cde2676c793407ef
DmitryDemenkov <demon.krut2002@mail.ru>
2022-05-29 09:35:29 +0300

    Добавлен класс модели фильма - Film

---
+++ OnlineCinema.Web/Models/Film.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineCinema.Web.Models
+{
+    public class Film
+    {
+        public Film(int id, string title, string category, DateTime releaseDate)
+        {
+            Id = id;
+            Title = title;
+            Category = category;
+            ReleaseDate = releaseDate;
+        }
+
+        public Film(int id, string title, string category, string annotation, DateTime releaseDate,
+                            int purchasePrise, int rentalPrice, int rentalDuration)
+        {
+            Id = id;
+            Title = title;
+            Category = category;
+            Annotation = annotation;
+            ReleaseDate = releaseDate;
+            PurchasePrice = purchasePrise;
+            RentalPrice = rentalPrice;
+            RentalDuration = rentalDuration;
+        }
+
+        public int Id { get; private set; }
+
+        public string Title { get; private set; }
+
+        public string Category { get; private set; }
+
+        public string Annotation { get; private set; }
+
+        public DateTime ReleaseDate { get; private set; }
+
+        public int PurchasePrice { get; private set; }
+
+        public int RentalPrice { get; private set; }
+
+        public int RentalDuration { get; private set; }
+
+    }
+}
