3c4bfa97dd2135781c9edcaff790f6dbd19bbf24
DmitryDemenkov <demon.krut2002@mail.ru>
2022-05-29 14:02:10 +0300

    Добавлен репозиторий пользователей

---
+++ OnlineCinema.Web/RepositoryInterfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OnlineCinema.Web.Models;
+
+namespace OnlineCinema.Web.RepositoryInterfaces
+{
+    interface IUserRepository : IRepository<User>
+    {
+        public User GetByLogin(string login, string password);
+
+        public User Append(User newUser);
+
+        public User Update(User user);
+
+        public void Delete(User user);
+    }
+}
