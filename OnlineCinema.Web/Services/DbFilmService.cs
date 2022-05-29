d866d0f8faf1589b62f1cdf53baa904aa14bc066
DmitryDemenkov <demon.krut2002@mail.ru>
2022-05-29 11:52:39 +0300

    Добавлен DbFilmService
    DbFilmService - служба для передачи информации о фильмах из репозиториев на веб-страницы

---
+++ OnlineCinema.Web/Services/DbFilmService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OnlineCinema.Web.Repositories;
+using OnlineCinema.Web.RepositoryInterfaces;
+using OnlineCinema.Web.Models;
+
+namespace OnlineCinema.Web.Services
+{
+    public class DbFilmService
+    {
+        public DbFilmService()
+        {
+            filmRepository = new MySqlDbFilmRepository();
+        }
+
+        private IFilmRepository filmRepository;
+
+        public IEnumerable<Film> GetPopularFilms(out string errorMsg)
+        {
+            try
+            {
+                IEnumerable<Film> films = filmRepository.GetPopular();
+                errorMsg = "No Mistakes";
+                return films;
+            }
+            catch (OnlineCinemaException exception)
+            {
+                errorMsg = exception.Message;
+                return null;
+            }
+        }
+    }
+}
