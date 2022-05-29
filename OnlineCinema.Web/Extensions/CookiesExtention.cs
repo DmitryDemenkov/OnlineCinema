803ac51e12bc3eff12c1a518d3e0b32830928572
DmitryDemenkov <demon.krut2002@mail.ru>
2022-05-29 15:05:54 +0300

    Добавлен CookiesExtention
    CookiesExtention - расширение для хранения в куки файлах объектов

---
+++ OnlineCinema.Web/Extensions/CookiesExtention.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace OnlineCinema.Web.Extensions
+{
+    public static class CookiesExtention
+    {
+        public static void Append<T> (this IResponseCookies cookies, string key, T value)
+        {
+            cookies.Append(key, JsonSerializer.Serialize(value));
+        }
+
+        public static bool TryGetValue<T>(this IRequestCookieCollection cookies, string key, out T value)
+        {
+            if (cookies.TryGetValue(key, out string stringValue))
+            {
+                value = JsonSerializer.Deserialize<T>(stringValue);
+                return true;
+            }
+            else
+            {
+                value = default;
+                return false;
+            }
+        }
+    }
+}
