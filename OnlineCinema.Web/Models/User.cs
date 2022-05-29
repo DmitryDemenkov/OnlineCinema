839ceaed26584c7ce01022ed57b96bee1f919c9a
DmitryDemenkov <demon.krut2002@mail.ru>
2022-05-29 13:55:20 +0300

    Добавлена модель пользователя User

---
+++ OnlineCinema.Web/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineCinema.Web.Models
+{
+    public class User
+    {
+        public User(int id, string login, string password, string email, DateTime birthDate) 
+        {
+            Id = id;
+            Login = login;
+            Password = password;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public int Id { get; private set; }
+
+        public string Login { get; private set; }
+
+        public string Password { get; private set; }
+
+        public string Email { get; private set; }
+
+        public DateTime BirthDate { get; private set; }
+    }
+}
