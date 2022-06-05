6ced7c4ed952de5fdf5ea2137537edbdfa6d3801
DmitryDemenkov <demon.krut2002@mail.ru>
2022-06-05 19:22:20 +0300

    Добавлена страница библиотеки пользователя

--- OnlineCinema.Web/Pages/Profile/Library.cshtml.cs
+++ OnlineCinema.Web/Pages/Profile/Library.cshtml.cs
@@ -4,17 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineCinema.Web.Models;
+using OnlineCinema.Web.Services;
+using OnlineCinema.Web.Extensions;
 
 namespace OnlineCinema.Web.Pages.Profile
 {
     public class LibraryModel : PageModel
     {
+        public LibraryModel(DbUserService userService)
+        {
+            UserService = userService;
+        }
+
+        public IEnumerable<FilmToLibrary> Films { get; private set; }
+
+        private DbUserService UserService;
+
         public IActionResult OnGet()
         {
-            if (!Request.Cookies.ContainsKey("User"))
-                return RedirectToPage("/Index");
+            if (Request.Cookies.TryGetValue<User>("User", out User user))
+            {
+                Films = UserService.GetUserLibrary(user, out int errorCode);
 
-            return Page();
+                if (errorCode != 0)
+                    return Redirect($"/Error?DbError={errorCode}");
+
+                return Page();
+            }
+            return RedirectToPage("/Index");
         }
     }
 }
