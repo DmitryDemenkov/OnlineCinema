9d0b5a50b772f4675bc4f81b9261b848112f15f3
DmitryDemenkov <demon.krut2002@mail.ru>
2022-06-05 17:05:33 +0300

    Добавлена информация о пользователе в личном кабинете

--- OnlineCinema.Web/Pages/Profile/Index.cshtml.cs
+++ OnlineCinema.Web/Pages/Profile/Index.cshtml.cs
@@ -4,17 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineCinema.Web.Models;
+using OnlineCinema.Web.Extensions;
 
 namespace OnlineCinema.Web.Pages.Profile
 {
     public class IndexModel : PageModel
     {
+        public User IdentifiedUser { get; private set; }
+
         public IActionResult OnGet()
         {
-            if (!Request.Cookies.ContainsKey("User"))
+            if (Request.Cookies.TryGetValue<User>("User", out User user))
+            {
+                IdentifiedUser = user;
+                return Page();
+            }
+            else
+            {
                 return RedirectToPage("/Index");
-
-            return Page();
+            }
         }
     }
 }
