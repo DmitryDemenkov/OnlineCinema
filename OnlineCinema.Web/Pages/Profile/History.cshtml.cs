242c34596986e869f8c4805d2556efc8a861349e
DmitryDemenkov <demon.krut2002@mail.ru>
2022-06-09 14:49:36 +0300

    Добавления страницы истории заказов

--- OnlineCinema.Web/Pages/Profile/History.cshtml.cs
+++ OnlineCinema.Web/Pages/Profile/History.cshtml.cs
@@ -4,17 +4,38 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineCinema.Web.Services;
+using OnlineCinema.Web.Models;
+using OnlineCinema.Web.Extensions;
 
 namespace OnlineCinema.Web.Pages.Profile
 {
     public class HistoryModel : PageModel
     {
+        public HistoryModel(DbUserService userService)
+        {
+            UserService = userService;
+        }
+
+        public IEnumerable<Order> Orders { get; private set; }
+
+        private DbUserService UserService;
+
         public IActionResult OnGet()
         {
-            if (!Request.Cookies.ContainsKey("User"))
-                return RedirectToPage("/Index");
+            if (Request.Cookies.TryGetValue<User>("User", out User user))
+            {
+                Orders = UserService.GetUsersHistory(user.Id, out int errorCode);
 
-            return Page();
+                if (errorCode != 0)
+                    return RedirectToPage($"/Error?DbError={errorCode}");
+
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("/Index");
+            }            
         }
     }
 }
